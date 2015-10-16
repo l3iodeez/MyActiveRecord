1a14838cbbceb9968816bb4ed94a0a319fed6c59
HDotson <l3iodeez@gmail.com>
2015-10-16 11:23:34 -0400

    Write my_attr_accessor

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,15 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      self.send(:define_method, name.to_sym) do
+        instance_variable_get("@#{name}".to_sym)
+      end
+
+
+
+      self.send(:define_method, "#{name}=") do |object|
+        instance_variable_set("@#{name}".to_sym, object)
+      end
+    end
   end
 end
