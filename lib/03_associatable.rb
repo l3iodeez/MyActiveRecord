6ae3b155e06e31de2a6463e0a7bab6f9edbcc2a8
HDotson <l3iodeez@gmail.com>
2015-10-16 17:13:44 -0400

    Write Associatable module part 1.

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,33 +10,65 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    options[:primary_key] ||= :id
+    options[:class_name] ||= name.to_s.camelize.singularize
+    options[:foreign_key] ||= "#{name.to_s.downcase}_id".to_sym
+    @primary_key = options[:primary_key]
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    options[:primary_key] ||= :id
+    options[:class_name] ||= name.to_s.camelize.singularize
+    options[:foreign_key] ||= "#{self_class_name.to_s.downcase}_id".to_sym
+    @primary_key = options[:primary_key]
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+
+    define_method(name) do
+      options_obj = BelongsToOptions.new(name, options)
+      parent_class = options_obj.model_class
+
+      f_key = self.send(options_obj.foreign_key)
+      parent = parent_class.where({:id => f_key })
+      return nil if parent.count < 1
+      parent.first
+    end
   end
 
   def has_many(name, options = {})
+      options_obj = HasManyOptions.new(name, self, options)
+
+    define_method(name) do
+      target_class = options_obj.model_class
+
+      p_key_value = self.send(options_obj.primary_key)
+      f_key = options_obj.foreign_key
+      options_obj.model_class.where({ f_key => p_key_value })
+    
+
+    end
+
+
     # ...
   end
 
@@ -46,5 +78,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
